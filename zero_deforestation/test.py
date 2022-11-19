53db62cad0795475e558feee8f54f99a3dd26b4b
agarcia <alejandro.garcia.ihs@gmail.com>
2022-11-19 21:13:15 +0100

    Add solution with train val set and final solution config.

--- zero_deforestation/test.py
+++ zero_deforestation/test.py
@@ -3,24 +3,29 @@
 import argparse
 import torch
 from tqdm import tqdm
-import data_loader.data_loaders as module_data
-import model.loss as module_loss
-import model.metric as module_metric
-import model.model as module_arch
+import zero_deforestation.dataset as module_data
+import zero_deforestation.data_loader.data_loaders as module_dataloader
+import zero_deforestation.data_loader.augmentations as module_augmentations
+import zero_deforestation.model.loss as module_loss
+import zero_deforestation.model.metric as module_metric
+import zero_deforestation.model.model as module_arch
 from parse_config import ConfigParser
 
 
 def main(config):
     logger = config.get_logger("test")
 
+    # set data augmentation
+    aug = config.init_obj("train_aug", module_augmentations, train=False)
+
+    # setup dataset instances
+    dataset = config.init_obj(
+        "train_dataset", module_data, transform=aug, return_label=True
+    )
+
     # setup data_loader instances
-    data_loader = getattr(module_data, config["data_loader"]["type"])(
-        config["data_loader"]["args"]["data_dir"],
-        batch_size=512,
-        shuffle=False,
-        validation_split=0.0,
-        training=False,
-        num_workers=2,
+    data_loader = config.init_obj(
+        "train_data_loader", module_dataloader, dataset=dataset, sampler=None
     )
 
     # build model architecture
@@ -46,14 +51,14 @@ def main(config):
     total_loss = 0.0
     total_metrics = torch.zeros(len(metric_fns))
 
+    model_outputs = []
+
     with torch.no_grad():
         for _, (data, target) in enumerate(tqdm(data_loader)):
             data, target = data.to(device), target.to(device)
             output = model(data)
 
-            #
-            # save sample images, or do something with output here
-            #
+            model_outputs.append(output)
 
             # computing loss, metrics on test set
             loss = loss_fn(output, target)
